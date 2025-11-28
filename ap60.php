ba8cbc613ec378b2d06c96d8946a2939d6390611
Miguel <michsa@alumnatflorida.es>
2025-11-28 10:19:39 +0100

    Ejercicio 2

--- ap60.php
+++ ap60.php
@@ -1,11 +1,23 @@
 <?php
-
+//DEFINICIÓN DE LA CLASE
 class Coche{
-    public $marca;
-    public $modelo;
+    private $marca;
+    private $modelo;
     
     public function mostrarInfo(){
-        echo "Coche: " . $marca . " " . $modelo;
+        echo "Coche: " . $this->marca . " " . $this->modelo;
+    }
+    public function getMarca(){
+        return $this->marca;
+    }
+    public function setMarca($marca){
+        $this->marca = $marca;
+    }
+    public function getModelo(){
+        return $this->modelo;
+    }
+    public function setModelo(){
+        $this->modelo = $modelo;
     }
 }
 
